3eb1be12049c6b5a0350ad017a88828210cf2acb
Sibghat776 <ullahsibghat786@gmail.com>
2025-01-01 19:40:15 +0500

    On progresssss ha

---
+++ Login/app.js
@@ -0,0 +1,8 @@
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+let loginBtn = document.querySelector("button")
+
+loginBtn.addEventListener("click", () => {
+    console.log("Chala login Wala")
+    
+})
--- Signup/app.js
+++ Signup/app.js
@@ -4,15 +4,19 @@ let email = document.querySelector("#email")
 let firstName = document.querySelector("#firstName")
 let lastName = document.querySelector("#lastName")
 
-
 myBtn.addEventListener("click", () => {
-    
-let users = [{
-    password: password.value,
-    firstName: firstName.value,
-    email: email.value,
-    lastName: lastName.value
-}]
-    console.log("Me chala")
-    localStorage.setItem("users" , JSON.stringify(users))
+    if (password.value.length < 8) {
+        alert("Your Password Should have 8 characters!!!")
+    } else {
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        let user = {
+            password: password.value,
+            firstName: firstName.value,
+            email: email.value,
+            lastName: lastName.value
+        }
+        users.push(user)
+        console.log("Me chala")
+        localStorage.setItem("users", JSON.stringify(users))
+    }
 })
