04c93e42e06982cde21f2e9868feda417dabd0d5
Sibghat776 <ullahsibghat786@gmail.com>
2025-01-04 07:38:05 +0500

    Finally Project done

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-let myBtn = document.querySelector("button")
-let password = document.querySelector("#password")
-let confirmPassword = document.querySelector("#confirmPassword")
-let email = document.querySelector("#email")
-let userName = document.querySelector("#userName")
+// let myBtn = document.querySelector("button")
+// let password = document.querySelector("#password")
+// let confirmPassword = document.querySelector("#confirmPassword")
+// let email = document.querySelector("#email")
+// let userName = document.querySelector("#userName")
 
-console.log(password.value)
-console.log(confirmPassword.value)
+// console.log(password.value)
+// console.log(confirmPassword.value)
 
-myBtn.addEventListener("click", () => {
+// myBtn.addEventListener("click", () => {
+//     if (userName.value == '' || password.value.length < 8 || password.value != confirmPassword.value || email.value == '') {
+//         alert("Please fill all fields correctly!");
+//     } else {
+//         let users = JSON.parse(localStorage.getItem("users")) || [];
+//         let user = {
+//             userName: userName.value,
+//             password: password.value,
+//             email: email.value,
+//         };
+//         users.push(user);
+//         localStorage.setItem("users", JSON.stringify(users));
+//         alert("User registered successfully!");
+//         setTimeout(() => {
+//             window.location.href = './Login/index.html'
+//         }, 1000);
+//         userName.value = '';
+//         password.value = '';
+//         email.value = '';
+//         confirmPassword.value = '';
+//     }
+// });
+
+let myBtn = document.querySelector("button");
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirmPassword");
+let email = document.querySelector("#email");
+let userName = document.querySelector("#userName");
+
+myBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // Default behavior روکنے کے لیے
+
     if (userName.value == '' || password.value.length < 8 || password.value != confirmPassword.value || email.value == '') {
         alert("Please fill all fields correctly!");
     } else {
@@ -17,16 +48,22 @@ myBtn.addEventListener("click", () => {
             password: password.value,
             email: email.value,
         };
+
         users.push(user);
         localStorage.setItem("users", JSON.stringify(users));
+
         alert("User registered successfully!");
+
         setTimeout(() => {
-            window.location.href = './Login/index.html'
+            window.location.href = './Login/index.html';
+            // Uncomment the below line if the issue persists
+            // window.location.reload();
         }, 1000);
+
+        // Clear the fields
         userName.value = '';
         password.value = '';
         email.value = '';
         confirmPassword.value = '';
     }
 });
-
